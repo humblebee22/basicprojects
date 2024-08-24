35598c1ec95da98e96c8ff507f60d9f89597070e
Kartik <kartikg2904@gmail.com>
2024-08-24 18:27:10 +0530

    non javascript functional project
    just tested stylish buttons using tl

---
+++ signatureapp/sign.js
@@ -0,0 +1,9 @@
+const colorpicker=document.getElementById
+("colorpicker");
+const canvascolor=document.getElementById("canvascolor");
+const canvas=document.getElementById("myCanvas");
+const saveButton=document.getElementById("savebutton");
+const retrievebutton =document.getElementById("Retreivebutton");
+const fontPicker=document.getElementById("fontSize");
+const ctx = canvas.getContext('2d');
+
